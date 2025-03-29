4690101d1e98eb275bc7148038f0f659bd59d4a3
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-03-29 19:02:41 +0530

    create my text field.dart file

---
+++ lib/components/my_text_feild.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyTextFeild extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  final bool isEnabled;
+
+  const MyTextFeild(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText,
+      required this.isEnabled});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: TextField(
+        decoration: InputDecoration(labelText: hintText),
+        enabled: isEnabled,
+        controller: controller,
+        obscureText: obscureText,
+      ),
+    );
+  }
+}
