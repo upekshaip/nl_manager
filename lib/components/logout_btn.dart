b1f298f7883c9287c27062a5f73897c3472b3179
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-04-02 17:28:14 +0530

    add a logout botton

---
+++ lib/components/logout_btn.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:nlmanager/tasks/course_state.dart';
+import 'package:nlmanager/tasks/downloader_state.dart';
+import 'package:nlmanager/tasks/session_state.dart';
+import 'package:provider/provider.dart';
+
+class LogoutBtn extends StatelessWidget {
+  const LogoutBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer3<SessionStateProvider, CourseStateProvider, DownloadStateProvider>(
+      builder: (context, mySession, myCourse, myDownloader, child) => GestureDetector(
+        onTap: () {
+          mySession.logOut(myCourse, myDownloader);
+          Navigator.of(context).pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+          Navigator.pushNamed(context, '/login');
+        },
+        child: Container(
+          width: 90,
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(color: Colors.red.shade900, borderRadius: BorderRadius.circular(30), border: Border.all(color: Colors.grey.shade800)),
+          child: const Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                  size: 15,
+                ),
+                Text(
+                  "Logout",
+                  style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
