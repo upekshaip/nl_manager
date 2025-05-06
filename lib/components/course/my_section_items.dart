a37436fbcb27a87365fc8d03a842863880ca544d
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-05-06 12:10:04 +0530

    Create my section items.dart 👌

---
+++ lib/components/course/my_section_items.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nlmanager/tasks/helpers.dart';
+
+class MySectionItem extends StatelessWidget {
+  final dynamic sectionContents;
+  const MySectionItem({super.key, required this.sectionContents});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        for (var sectionContent in sectionContents)
+          ListTile(
+            trailing: MyHelper()
+                .getIcons(sectionContent["ext"], sectionContent["url"]),
+            title: Text(sectionContent["file_name"],
+                style: TextStyle(fontSize: 12, color: Colors.grey.shade300)),
+          ),
+      ],
+    );
+  }
+}
