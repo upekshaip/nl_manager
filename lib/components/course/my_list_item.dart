34339f6a1cedf606d730940be590303a4c224566
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-05-24 00:59:00 +0530

    Changes in my_list_item.dart file

--- lib/components/course/my_list_item.dart
+++ lib/components/course/my_list_item.dart
@@ -1,74 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:nlmanager/components/course/my_section_items.dart';
 
-class MyCourseList extends StatefulWidget {
-  final List courses;
+class MyCourseListItem extends StatelessWidget {
+  final dynamic sections;
+  const MyCourseListItem({super.key, required this.sections});
 
-  const MyCourseList({super.key, required this.courses});
-
-  @override
-  State<MyCourseList> createState() => _MyCourseListState();
-}
-
-bool checkIsExpanded(dynamic item) {
-  if (item.containsKey("isExpanded")) {
-    return !item["isExpanded"];
-  }
-  return false;
-}
-
-class _MyCourseListState extends State<MyCourseList> {
   @override
   Widget build(BuildContext context) {
-    var courses = widget.courses;
-    return Expanded(
-      child: SingleChildScrollView(
-        child: ExpansionPanelList(
-          dividerColor: Colors.grey.shade600,
-          expansionCallback: (int index, bool isExpanded) {
-            setState(() {
-              courses[index]["isExpanded"] = !isExpanded;
-            });
-          },
-          children: courses.map<ExpansionPanel>((dynamic course) {
-            return ExpansionPanel(
-                backgroundColor: Colors.black,
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(course["fullname"],
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey.shade300)),
-                        Text(course["coursecategory"],
-                            style: TextStyle(
-                                fontSize: 12, color: Colors.grey.shade400)),
-                        const SizedBox(height: 8),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: LinearProgressIndicator(
-                                value: (course["progress"] as int) / 100,
-                                backgroundColor: Colors.grey.shade700,
-                                color: const Color.fromARGB(255, 0, 199, 10),
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            Text('${course["progress"]}%',
-                                style: TextStyle(color: Colors.grey.shade400)),
-                          ],
-                        ),
-                      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          for (var section in sections)
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              margin: const EdgeInsets.only(bottom: 8),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade900,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 4),
+                  Text(
+                    "• ${section["section_name"]}",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade300,
                     ),
-                  );
-                },
-                body: MyCourseListItem(sections: course["contents"]),
-                isExpanded: checkIsExpanded(course),
-                canTapOnHeader: true);
-          }).toList(),
-        ),
+                  ),
+                  MySectionItem(sectionContents: section["section_content"]),
+                  const SizedBox(height: 4),
+                ],
+              ),
+            ),
+        ],
       ),
     );
   }
